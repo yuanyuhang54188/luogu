68cca8d51a26e752c412d860526877e0b92a2163
袁宇杭 <yuanyuhang54188@163.com>
2024-12-04 11:36:15 +0800

    just another change

--- Luogu-User-1100140/Luogu-Discuss-1011321/Hand-Write-Heap/original - P1090-hack-1011321.cpp
+++ Luogu-User-1100140/Luogu-Discuss-1011321/Hand-Write-Heap/original - P1090-hack-1011321.cpp
@@ -1,53 +1,56 @@
 //取堆中次小值时没考虑len==2的情况,已修正
 #include <bits/stdc++.h>
 using namespace std;
-int a[10005], len, n;
-void extract() {
+int h[10005], len, n;
+
+void del() {
     int p = 1;
-    a[1] = a[n];
+    h[1] = h[len];
     len--;
     int s = p * 2;
     while ( s <= n ) {
-        if ( a[s] > a[s + 1] && s < n ) {
+        if ( h[s] > h[s + 1] && s < n ) {
             s++;
         }
-        if ( a[s] < a[p] ) {
-            swap ( a[s], a[p] );
+        if ( h[s] < h[p] ) {
+            swap ( h[s], h[p] );
             p = s;
             s *= 2;
         } else {
             break;
         }
     }
+    return;
 }
 void insert ( int val ) {
-    a[++len] = val;
+    h[++len] = val;
     int s = len / 2, p = len;
     while ( s >= 1 ) {
-        if ( a[s] > a[p] ) {
-            swap ( a[s], a[p] );
+        if ( h[s] > h[p] ) {
+            swap ( h[s], h[p] );
             p = s;
             s = p / 2;
         } else {
             break;
         }
     }
+    return;
 }
 int main() {
     scanf ( "%d", &n );
     len = n;
+    int x;
     for ( int i = 1; i <= n; i++ ) {
-        scanf ( "%d", &a[i] );
+        scanf ( "%d", &x );
+        insert ( x );
     }
-    sort ( a + 1, a + 1 + n );
     int ans = 0;
     for ( int i = 1; i <= n - 1; i++ ) {
-        int tmp2 = len >= 3 ? min ( a[2], a[3] ) : a[2];
-        ans += ( a[1] + tmp2 );   // 答案计数操作体重值
-        int tmp = ( a[1] + tmp2 );
-        extract();
-        extract();
-        insert ( tmp );
+        int tmp2 = h[1] + h[2];
+        ans += tmp2 ;
+        del();
+        del();
+        insert ( tmp2 );
     }
     printf ( "%d", ans );
     return 0;
---
+++ Luogu-User-1100140/P1553/1 - P1553-user-1100140.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+char a[50],b[50];
+bool c[50];
+bool flag;
+int main() {
+    scanf("%s", a);
+    for(int i = 0; i < strlen(a); i++) {
+        if(a[i] > '9' || a[i] < '0') {
+            if(a[i] == '%') {
+                int cnt = 0;
+                flag = true;
+                for(int j = i-1; j >= 0; j--) {
+                    b[cnt++] = a[j];
+                }
+                b[cnt++] = '%';
+            } else if(a[i] == '.') {
+                int cnt = 0;
+                flag = true;
+                for(int j = i-1; j >= 0; j--) {
+                    b[cnt++] = a[j];
+                }
+                b[cnt++] = '.';
+                for(int j = strlen(a)-1; j >= i+1; j--) {
+                    b[cnt++] = a[j];
+                }
+                break;
+            } else if(a[i] == '/') {
+                int cnt = 0;
+                flag = true;
+                for(int j = i-1; j >= 0; j--) {
+                    b[cnt++] = a[j];
+                }
+                b[cnt++] = '/';
+                for(int j = strlen(a)-1; j >= i+1; j--) {
+                    b[cnt++] = a[j];
+                }
+                break;
+            }
+        }
+    }
+    if(!flag) {
+        bool flag2 = true;
+        for(int i = strlen(a)-1; i >= 0; i--) {
+            if(flag2 && a[i] == '0') continue;
+            else if(a[i]!='0' && flag2) flag2 = false;
+            printf("%c", a[i]);
+        }
+    } else {
+        bool flag2 = true;
+        int len = 0,tmp = strlen(b);
+        for(int i = tmp-1; i >= 0; i--) {
+            len = i;
+            if(b[i] < '0' || b[i] > '9') break;
+        }
+        for(int i = 0; i <= len-1; i++) {
+            if(i != len-1 && flag2 && b[i] == '0') continue;
+            else if(b[i] != '0' && flag2) flag2 = false;
+            printf("%c", b[i]);
+        }
+        printf("%c", b[len]);
+        if (b[len] =='.') {                         // 特殊处理, 删掉的是后缀0, 不是前缀0
+            c[strlen(b)] = false;
+            for (int i = strlen(b) - 1; i > len; i--) {
+                if (b[i] == '0') c[i] = c[i + 1];
+                else c[i] = true;
+            }
+            for (int i = len+1; i < strlen(b); i++) {
+                if (c[i]) printf("%c", b[i]);
+            }
+        } else {
+            flag2 = true;
+            for(int i = len+1; i < strlen(b); i++) {
+                if(i != strlen(b) - 1 && flag2 && b[i] == '0') continue;
+                else if(b[i] != '0' && flag2) flag2 = false;
+                printf("%c", b[i]);
+            }
+        }
+    }
+}
